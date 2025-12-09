0d6b4016cdbc97e715e6aef15920eb039a756c6c
ranjan079 <maniranjan2062@gmail.com>
2025-12-09 22:18:11 +0545

    fifth day

---
+++ 5.py
@@ -0,0 +1,78 @@
+# Write a program to:
+# Take n numbers from the user and store them in a list
+# Create a new list containing only the squared values of each number
+# Print the original list and the squared list
+
+# a=int(input("Enter the value of n::::"))
+# a=[int(input("Enter value:::")) for _ in range(a)]
+# c=[]
+# for i in a:
+#     c.append(i*i)
+# print(c)
+
+
+# Write a Python program to:
+# Take n numbers into a list
+# Find the min and max values
+# Normalize each number using the formula:
+# normalized​
+# Print both original and normalized lists
+
+# a=int(input("Enter value of n:::"))
+# a=[int(input("Enter value:::")) for _ in range(a)]
+# ma=max(a)
+# mi=min(a)
+# c=[]
+# for i in a:
+#     r=(i-mi)/(ma-mi)
+#     c.append(r)
+# print(a)
+# print(c)
+
+# Write a program to calculate:
+# Mean (Average)
+# Median
+# Mode
+
+# import statistics as st
+# a=[10,20,30,40,50,20,10,20]
+# print("Mean:",st.mean(a))
+# print("Median:",st.median(a))
+# print("Mode:",st.mode(a))
+
+
+
+# !!!!!!!!!!!funtion!!!!
+# def add(a,b):
+#     return (a+b)
+# print(add(2,3))
+
+# def ty(name):
+#     print(f"Name:{name}")
+# ty("ranjan")
+
+
+# Write a function count_numbers(lst) that takes a list of numbers as input.
+# The function should count how many numbers are:
+# Positive
+# Negative
+# Zero
+# Return these counts and print them.
+# Ask the user to enter n numbers and store them in a list to pass to the function.
+
+def count(a):
+    pcount,ncount,count=0,0,0
+    for i in a:
+        if i>0:
+            pcount +=1
+        elif i<0:
+            ncount +=1
+        else:
+            count=1
+    return pcount,ncount,count
+n=int(input("ENter value of n"))
+a=[int(input("ENter value:")) for _ in range(n)]
+positive,negative,zero=count(a)
+print("positive numbers total::",positive)
+print("negative numbers total::",negative)
+print(zero)
