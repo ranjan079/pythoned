80a44b583abc836b26ac6e4664a4b899d689cc89
ranjan079 <maniranjan2062@gmail.com>
2025-12-13 22:01:27 +0545

    seventh day

--- 7.py
+++ 7.py
@@ -28,18 +28,53 @@
 # Non-numeric values in the list
 # Always return a number (or a meaningful message).
 
-a=list(map(int,input("Enter number::").split()))
-# a=int(input("Enter value ::"))
-# a=[int(input("ENter the value::")) for _ in range (a)]
-def avg(a):
-    sum=0
-    j=0
-    try:
-        for i in a:
-            j=len(a)
-            sum=sum+i
-        return (sum/j)
-    except ZeroDivisionError:
-        print("Enter valid number.")
-print("Average is :",avg(a))
+# a=list(map(int,input("Enter number::").split()))
+# # a=int(input("Enter value ::"))
+# # a=[int(input("ENter the value::")) for _ in range (a)]
+# def avg(a):
+#     sum=0
+#     j=0
+#     try:
+#         for i in a:
+#             j=len(a)
+#             sum=sum+i
+#         return (sum/j)
+#     except ZeroDivisionError:
+#         print("Enter valid number.")
+# print("Average is :",avg(a))
 
+# Write a function factorial(n) that calculates the factorial of a number n
+# Input the number from the user and print the factorial using the function
+
+# def fact(a):
+#     for i in range (1,a):
+#         a=a*i
+#     return a
+# a=int(input("Enter number :"))
+# print("The factorial is :::",fact(a))
+
+# Write a function count_vowels(s) that takes a string s as input
+# Counts the number of vowels (a, e, i, o, u) in the string
+# Returns the coun
+
+
+# a=input("Enter string::")
+# def coun(a):
+#     count=0
+#     for i in a:
+#         if i.lower() in 'aeiou':
+#             count +=1
+#     return count
+# print("Total vowel count is ::",coun(a))
+
+# Write a function unique_sorted(lst) that:
+# Takes a list of numbers as input
+# Returns a sorted list of unique elements
+
+def sort(a):
+    l=set(a)
+    print(l)
+    m=sorted(l)
+    return m
+a=list(map(int,input("enter number seperated by space::").split()))
+print("SOrted number is::",sort(a))
